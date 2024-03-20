8a25f4ec1201c5554683980a28ad5a2d39ef7dcc
joancoll <joancoll@inspladelestany.cat>
2024-03-20 14:17:31 +0100

    1st commit

--- app/src/main/java/cat/dam/andy/swipesound_compose/MainActivity.kt
+++ app/src/main/java/cat/dam/andy/swipesound_compose/MainActivity.kt
@@ -1,19 +1,20 @@
 package cat.dam.andy.swipesound_compose
 
-import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import kotlin.math.abs
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
@@ -22,16 +23,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import cat.dam.andy.swipesound_compose.ui.theme.SwipeSound_composeTheme
-import java.lang.Math.abs
+import cat.dam.andy.swipesound_compose.viewmodel.MyViewModel
+import cat.dam.andy.swipesound_compose.viewmodel.Sound
 
 var detectGestures = true
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val myViewModel: MyViewModel by viewModels()
         setContent {
             SwipeSound_composeTheme {
                 // A surface container using the 'background' color from the theme
@@ -39,7 +43,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SwipeDetector()
+                    SwipeDetector(myViewModel)
                 }
             }
         }
@@ -47,25 +51,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SwipeDetector() {
-    val context = LocalContext.current
-    val backgroundColor = remember { mutableStateOf(Color.White) }
-    val swipeLeftSound = MediaPlayer.create(context, R.raw.left)
-    val swipeRightSound = MediaPlayer.create(context, R.raw.right)
-    val swipeUpSound = MediaPlayer.create(context, R.raw.up)
-    val swipeDownSound = MediaPlayer.create(context, R.raw.down)
-    val instructionsSound = MediaPlayer.create(context, R.raw.instructions)
-
+fun SwipeDetector(myViewModel: MyViewModel = viewModel()) {
+    val backgroundColor = myViewModel.backgroundColor.value
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Swipe to change color and play sound")
-        Button(content = { Text("Instructions") }, onClick = { playSound(instructionsSound) })
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Background Music")
+            Switch(checked = myViewModel.isMusicPlaying.value, onCheckedChange = { isChecked ->
+                myViewModel.isMusicPlaying.value = isChecked
+                myViewModel.toggleBackgroundMusic()
+            })
+            Button(content = { Text("Instructions") }, onClick = { myViewModel.playSound(Sound.INSTRUCTIONS) })
+        }
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
+                .background(backgroundColor)
                 .pointerInput(Unit) {
                     if (detectGestures) {
                         detectDragGestures { change, dragAmount ->
@@ -78,34 +88,29 @@ fun SwipeDetector() {
                                     // Horizontal swipe
                                     if (directionX > 0) {
                                         // Swipe Right
-                                        backgroundColor.value = Color.Green
-                                        playSound(swipeRightSound)
+                                        myViewModel.backgroundColor.value = Color.Green
+                                        myViewModel.playSound(Sound.SWIPE_RIGHT)
                                     } else {
                                         // Swipe Left
-                                        backgroundColor.value = Color.Red
-                                        playSound(swipeLeftSound)
+                                        myViewModel.backgroundColor.value  = Color.Red
+                                        myViewModel.playSound(Sound.SWIPE_LEFT)
                                     }
                                 } else {
                                     // Vertical swipe
                                     if (directionY > 0) {
                                         // Swipe Down
-                                        backgroundColor.value = Color.Blue
-                                        playSound(swipeDownSound)
+                                        myViewModel.backgroundColor.value  = Color.Blue
+                                        myViewModel.playSound(Sound.SWIPE_DOWN)
                                     } else {
                                         // Swipe Up
-                                        backgroundColor.value = Color.Yellow
-                                        playSound(swipeUpSound)
+                                        myViewModel.backgroundColor.value  = Color.Yellow
+                                        myViewModel.playSound(Sound.SWIPE_UP)
                                     }
                                 }
                             }
                         }
                     }
                 }
-                .background(backgroundColor.value)
         )
     }
-}
-
-fun playSound(sound: MediaPlayer) {
-    sound.start()
 }
---
+++ app/src/main/java/cat/dam/andy/swipesound_compose/viewmodel/MyViewModel.kt
@@ -0,0 +1,62 @@
+package cat.dam.andy.swipesound_compose.viewmodel
+
+import android.app.Application
+import android.media.MediaPlayer
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import cat.dam.andy.swipesound_compose.R
+
+enum class Sound {
+    SWIPE_LEFT,
+    SWIPE_RIGHT,
+    SWIPE_UP,
+    SWIPE_DOWN,
+    INSTRUCTIONS,
+    MUSIC_BACKGROUND
+}
+
+class MyViewModel(application: Application) : AndroidViewModel(application) {
+
+    val isMusicPlaying = mutableStateOf(true)
+    var backgroundColor = mutableStateOf(Color.Cyan)
+
+    private val sounds = mapOf(
+        Sound.SWIPE_LEFT to MediaPlayer.create(application, R.raw.left),
+        Sound.SWIPE_RIGHT to MediaPlayer.create(application, R.raw.right),
+        Sound.SWIPE_UP to MediaPlayer.create(application, R.raw.up),
+        Sound.SWIPE_DOWN to MediaPlayer.create(application, R.raw.down),
+        Sound.INSTRUCTIONS to MediaPlayer.create(application, R.raw.instructions),
+        Sound.MUSIC_BACKGROUND to MediaPlayer.create(application, R.raw.music_separation_by_williamking).apply {
+            isLooping = true
+            setVolume(0.2f, 0.2f)
+            if (isMusicPlaying.value) start()
+        }
+    )
+
+    fun playSound(sound: Sound) {
+        sounds[sound]?.let {
+            if (!it.isPlaying) {
+                it.start()
+            }
+        }
+    }
+
+    fun toggleBackgroundMusic() {
+        viewModelScope.launch {
+            if (isMusicPlaying.value) {
+                sounds[Sound.MUSIC_BACKGROUND]?.start()
+            } else {
+                sounds[Sound.MUSIC_BACKGROUND]?.pause()
+            }
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        // Allibera els recursos dels MediaPlayer quan el ViewModel es destrueix
+        sounds.values.forEach { it.release() }
+    }
+}
